aa25133e0b7db0d2c9ea60b3af001a87ca09318a
sahil0233 <73436999+sahil0233@users.noreply.github.com>
2025-02-07 00:00:48 +0530

    changed phone number

--- src/components/WhatsappChat.jsx
+++ src/components/WhatsappChat.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const WhatsappChat = () => {
   return (
     <div>
-    <a className='fixed bottom-4 left-4 p-2 bg-white hover:bg-blue-500 border-2 rounded-xl' href='https://wa.me/9829179622' target='_blank'>
+    <a className='fixed bottom-4 left-4 p-2 bg-white hover:bg-blue-500 border-2 rounded-xl' href='https://wa.me/8239279999' target='_blank'>
         <img className='w-8'src='https://firebasestorage.googleapis.com/v0/b/ajmerstore-7d3af.appspot.com/o/assets%2FwhatsappIcon.svg?alt=media&token=0c904fa0-c190-4019-aa46-79659d1a02cd' />
         </a>
       
